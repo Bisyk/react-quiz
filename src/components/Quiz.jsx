9e3bffc52a6bc7a05aac564f8db9d93ca4c66f08
Bisyk <bskyrslv@gmail.com>
2024-01-01 22:53:44 +0200

    Fixing an error when the question did not switch after the time expired

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
-import { useCallback, useState } from "react";
+import { useState, useCallback } from 'react';
 
-import QUESTIONS from "../questions";
-import Question from "./Question";
-import Summary from "./Summary";
+import QUESTIONS from '../questions.js';
+import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -13,18 +13,19 @@ export default function Quiz() {
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ) {
-    setUserAnswers((prevAnswers) => {
-      return [...prevAnswers, selectedAnswer];
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
     });
   },
   []);
 
-  const handleSkipAnswer = useCallback(() => {
-    () => handleSelectAnswer(null);
-  }, [handleSelectAnswer]);
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
-    return <Summary userAnswers={userAnswers} />;
+    return <Summary userAnswers={userAnswers} />
   }
 
   return (
